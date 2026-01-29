03c5bd6e7085dd5f6ea21b11b8faecdda366305b
jsu <sujunjie@gmail.com>
2026-01-29 12:02:50 +0800

    feats(3): (1) added ability to load and save db_file, (2) aligned match case logic, when valid cmd, but unsuccessful op, (3) added convenience functions, SHOWDB, CLEARDB, SAVEDB to display on server terminal

--- mini_db.py
+++ mini_db.py
@@ -1,15 +1,31 @@
+import json
 import signal
 import socket
 import sys
 import threading
 
 db = {}  # in-memory key-value 'database'
-
+filename = ""
 
 def signal_handler(sig, frame):
     # save_db()
     sys.exit(0)
 
+def load_db(filename):
+    try:
+        with open(filename, 'r') as f:
+            # list comprehension
+            db = dict(line.strip().split(' ') for line in f if ' ' in line)
+        return db
+    except FileNotFoundError:
+        print("Expected usage: python3 mini_db.py <port> <db_file>")
+        print("Error: <db_file> not found")
+        sys.exit(1)
+
+def save_db(filename):
+    with open(filename, 'w') as f:
+        for key, val in db.items():
+            f.write(f"{key} {val}\n")
 
 def start_server(port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -19,10 +35,9 @@ def start_server(port):
 
         while True:
             conn, addr = s.accept()
-            # instead of multiplexing, ie. select(), use threading instead
+            # alternatives to threading? multiplexing with select, asyncio ?
             client_thread = threading.Thread(target=handle_client, args=(conn, addr))
             client_thread.start()
-
 
 def handle_client(conn, addr):
     with conn:
@@ -35,55 +50,74 @@ def handle_client(conn, addr):
             if not msg:
                 continue
 
-            # Assume at least <cmd> and <key> to be present
-            # If cmd == POST, then assume <val> to be present as well
             cmd = msg[0].upper()
-            # RHS is conditional expr: v1, if <condition> True, else v2
+            # RHS for key & val below, is conditional expr, aka "ternary" 
+            # value_1, if <condition> True, else value_2
             key = msg[1] if len(msg) > 1 else None
             val = msg[2] if len(msg) > 2 else None
 
-            # print(f"{cmd} {key} {val}") # sanity check
-
+            # NOTE. cmd is enforced to be UPPERCASE! 
             if cmd == "GET":
                 mode = 1
             elif cmd == "POST":
                 mode = 2
             elif cmd == "DELETE":
                 mode = 3
-            else:
+            elif cmd == "SHOWDB":
                 mode = 4
+            elif cmd == "CLEARDB"
+                mode = 5
+            elif cmd == "SAVEDB"
+                mode = 6
+            else:
+                mode = 7
 
-                # separate identifying the 'cmd' (above), from each cmd's implementation (below)
+            # separate identifying the 'cmd' (above), 
+            # from each cmd's implementation (below)
 
             # NOTE. "match" is only available v3.10 & later
             match mode:
-                case 1:
+                case 1: # GET
                     if key and key in db:
                         response = f"0 {db[key]}\n"
                     else:
                         response = "1\n"
-                case 2:
+                case 2: # POST
                     if key and val:
                         db[key] = val
-                        # save_db()
                         response = "0\n"
-                case 3:
+                    else:
+                        response = "1\n"
+                case 3: # DELETE
                     if key and key in db:
                         del db[key]
                         response = "0\n"
                     else:
                         response = "1\n"
-                case 4:
+                case 4: # SHOWDB
+                    print(json.dumps(db, indent=2)) # display at server terminal
+                    response = "0\n"
+                case 5: # CLEARDB
+                    db.clear()
+                    response = "0\n"
+                case 6: # SAVEDB
+                    save_db(filename)
+                    response = "0\n"
+                case 7:
                     response = "2\n"  # default response, for invalid/unhandled input
-
             conn.sendall(response.encode("utf-8"))  # send feedback back to client
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:  # python3 mini_db.py <port>
+        print("Expected usage: python3 mini_db.py <port> <db_file>")
         sys.exit(1)
 
     signal.signal(signal.SIGINT, signal_handler)
 
     port = int(sys.argv[1])
+    filename = sys.argv[2]
+
+    db = load_db(filename)
+    
     start_server(port)
